8003767fdbc32c23e86adac252bb600d8c155aac
valeeprakhon <53828611+valeeprakhon@users.noreply.github.com>
2020-08-18 20:01:18 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  ScrollView,
+} from 'react-native';
+
+import Item from './Item'
+import Item2 from './Item2'
+
+let initialArr = [{ id: 1, color: "blue",name: "text1"},{id: 2,color: "red",name: "text2"}];
+
+export default class Login extends Component {
+  constructor(){
+    super();
+     this.state = {
+        status: 0,
+        txtStatus:"Login",
+        items:[]
+    };
+  }
+  
+
+
+
+  render(props) {
+ 
+    
+    return (
+      <View>
+       
+
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#DDDDDD',
+    margin: 6,
+    padding: 10,
+  },textLogin:{
+    color:'red',
+    fontWeight:"bold",
+    fontSize:20,
+    padding:8,
+    alignSelf:"center"
+  },textLogout:{
+    fontWeight:"bold",
+    fontSize:20,
+    padding:8,
+    alignSelf:"center"
+  }
+});
